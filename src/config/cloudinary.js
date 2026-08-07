ebbe0f83b0c14d75982161c1c4cf21131a91c933
guillermo acciardi <gacciardi@gmail.com>
2026-08-07 12:54:04 -0300

    Corregir configuracion Cloudinary

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -4,8 +4,8 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-   signature_algorithm: "sha256"
-  secure: true
+  secure: true,
+  signature_algorithm: "sha256"
 });
 
 console.log("CLOUDINARY CONFIG:");
