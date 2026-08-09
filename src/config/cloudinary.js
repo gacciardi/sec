b6fde84ca6510b2df1115af69574681528ac0cfc
guillermo acciardi <gacciardi@gmail.com>
2026-08-09 19:22:55 -0300

    Log Cloudinary

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -4,11 +4,13 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-  signature_algorithm: "sha256"
+  secure: true
 });
 
-console.log("CLOUDINARY CONFIG:");
-console.log(cloudinary.config());
+console.log("========== CLOUDINARY ==========");
+console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
+console.log("SECRET LARGO:", (process.env.CLOUDINARY_API_SECRET || "").length);
+console.log("================================");
 
 module.exports = cloudinary;
